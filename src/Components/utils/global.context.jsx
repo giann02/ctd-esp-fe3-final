257f4d22092716364b9849b6af6b58bd6a3cf4f4
giann02 <gianlucapanigatti02@gmail.com>
2022-12-10 13:05:57 -0300

    Global context finished

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,15 +1,45 @@
 import { createContext } from "react";
+import { useState } from "react";
 
-export const initialState = {theme: "", data: []}
 
-export const ContextGlobal = createContext(undefined);
+export const initialState = {
+    themes: {
+        light: {
+            backgroundNavbar:"rgb(223, 219, 219)",
+            backgroundHome:"white",
+            backgroundFooter:"#cb3234",
+            color:"black"
+        },
+        dark: {
+            backgroundNavbar:"#161616",
+            backgroundHome:"#12121296",
+            backgroundFooter:"#161616",
+            color:"white"
+        }
+    },
+    favs:JSON.parse(localStorage.getItem("favs")||"[]")
+}
 
-export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+export const ContextGlobal = createContext(initialState); // Exporta context global
 
-  return (
-    <ContextGlobal.Provider value={{}}>
-      {children}
-    </ContextGlobal.Provider>
-  );
+export const ContextProvider = ({ children }) => { // Exporta provider del contexto global
+
+    // Context de los Themes
+    const [Theme, setTheme] = useState(initialState.themes.light)
+    const handleThemeChange = () => {
+    if(Theme === initialState.themes.dark){
+        setTheme(initialState.themes.light)
+    } 
+    else if(Theme === initialState.themes.light){
+        setTheme(initialState.themes.dark)
+    } 
+    }
+    // This is the context of the favorites
+    const [Favs, setFavs] = useState(initialState.favs)
+
+    return (
+        <ContextGlobal.Provider value={{Theme, handleThemeChange, Favs, setFavs}}>
+            {children}
+        </ContextGlobal.Provider>
+    );
 };
